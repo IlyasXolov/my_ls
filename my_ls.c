f7a2a02e177d9bd37e6902e6cdd9ace6b36f5759
Ilyas Xolov <125030510+IlyasXolov@users.noreply.github.com>
2023-02-13 09:37:42 +0500

    #define
    this is the code description------
    SPECIFICATIONS
    Write a programm called my_ls. Following the specifications of this man page.
    
    NAME
    my_ls -- list directory contents
    
    SYNOPSIS
    my_ls [-at] [file ...]
    
    DESCRIPTION
    For each operand that names a file of a type other than directory, my_ls displays its name as well as any requested, associated information. For each operand that names a file of type directory, my_ls displays the names of files contained within that directory, as well as any requested, associated information.
    
    If no operands are given, the contents of the current directory are displayed. If more than one operand is given, non-directory operands are displayed first; directory and non-directory operands are sorted separately and in lexicographical order.
    
    The following options are available:
    
    -a Include directory entries whose names begin with a dot (.).
    -t Sort by time modified (most recently modified first) before sorting the operands by lexicographical order.
    Requirements
    Your code must be compiled with the flags -Wall -Wextra -Werror.
    Hint(s)
    Watch out for memory leaks !
    You can test your code against memory errors by compiling with the debugging flags -g3 -fsanitize=address
    Global variables are strictly FORBIDDEN
    tv_sec AND tv_nsec are used for the -t options ;-)
    st_mtime is not what you want to use. You want to use st_mtim

---
+++ my_ls.c
@@ -0,0 +1,15 @@
+            #include "include.h"
+            #include "struct_in_print.h"
+            
+            STRUCTURA_LEN
+            MY_PRINT_TO_ALL_AND_NODE
+            STRCMP_MY
+            ADDLAST_FUNCTION
+            MY_SWAP_NODE
+            MY_SORT_A_T_AT
+            PERSONAL_QWASAR_G
+            IDENTEFIER_FILE_OR_DIR
+            FLAGES_H
+            MY_OPEN_FUNC_IN_LS
+            MY_LS
+            MAIN
